3f0e9d54cbd8f75bc536708ad6f670eda0e158a9
rostaspeter <166908807+rostaspeter@users.noreply.github.com>
2024-12-03 14:33:52 +0100

    Update teszt_Haromszog_integracios.py

--- teszt_Haromszog_integracios.py
+++ teszt_Haromszog_integracios.py
@@ -1,1 +1,15 @@
+import unittest
+from Haromszog import Haromszog
 
+class TestHaromszog(unittest.TestCase):
+     def test_szerkesztheto_modositasok(self):
+        h = Haromszog(5, 5, 5)
+        
+        h.SetB(6)
+        
+        self.assertEqual(h.Kerulet(), 16)
+
+
+if __name__ == '__main__':
+
+    unittest.main()
